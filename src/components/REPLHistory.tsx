e454f0bdedcec19ba08a1b41a0684e1095ace236
charlieolcott <charles_olcott@brown.edu>
2024-02-24 14:54:28 -0500

    feat: gearup code added

---
+++ src/components/REPLHistory.tsx
@@ -0,0 +1,20 @@
+import '../styles/main.css';
+
+interface REPLHistoryProps{
+    history : string[];
+    // TODO: Fill with some shared state tracking all the pushed commands
+}
+export function REPLHistory(props : REPLHistoryProps) {
+    const commandHistory = props.history;
+    return (
+        <div className="repl-history">
+            <p>Command history:</p>
+            {/* This is where command history will go */}
+            {/* TODO: To go through all the pushed commands... try the .map() function! */}
+            {props.history.map(
+                (command) =>
+                <p>{command}</p>
+            )}
+        </div>
+    );
+}
