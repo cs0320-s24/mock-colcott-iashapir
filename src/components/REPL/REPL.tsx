6f8ef308df99d36f7253a8672894419cef3452d9
charlieolcott <113263130+charlieolcott@users.noreply.github.com>
2024-03-01 17:19:22 -0500

    chore: remove gearup comment

--- src/components/REPL/REPL.tsx
+++ src/components/REPL/REPL.tsx
@@ -4,15 +4,6 @@ import { REPLHistory } from "../history/REPLHistory";
 import { REPLInput } from "./REPLInput";
 import { HistoryElement } from "../history/historyElement";
 
-/* 
-  You'll want to expand this component (and others) for the sprints! Remember 
-  that you can pass "props" as function arguments. If you need to handle state 
-  at a higher level, just move up the hooks and pass the state/setter as a prop.
-  
-  This is a great top level component for the REPL. It's a good idea to have organize all components in a component folder.
-  You don't need to do that for this gearup.
-*/
-
 /**
  * displays the REPLHistory, which contains all of the past command results, and displays
  * the REPLInput, which contains all of the user input functionality
